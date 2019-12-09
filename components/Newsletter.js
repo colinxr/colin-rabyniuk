404cc7b0ff78bd4d30640e371cf991d5a478d245
Colin Rabyniuk <colinxr@gmail.com>
2019-12-09 13:03:10 -0500

    check in files

---
+++ components/Newsletter.js
@@ -0,0 +1,7 @@
+const Newsletter = () => (
+  <div className="wrapper">
+    <iframe src="https://colinrabyniuk.substack.com/embed" frameBorder="0" scrolling="no"></iframe>
+  </div>
+)
+
+export default Newsletter
