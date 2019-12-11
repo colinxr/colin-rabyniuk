3f72d3bf1150eda0f1be851a70e48b6ffb68f3cb
Colin Rabyniuk <colin@madebyarticle.com>
2019-12-11 16:52:56 -0500

    updates

--- pages/journal.js
+++ pages/journal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Blog from '../components/Blog';
-import { arrowFunctionExpression } from '@babel/types';
 
 const Journal = () => (
   <Layout
