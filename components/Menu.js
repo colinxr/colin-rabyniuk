235ff0ac8741aec6386fa3f7513bb7fae1920a40
Colin Rabyniuk <colinxr@gmail.com>
2019-12-21 16:08:19 -0500

    link fixes in menu

--- components/Menu.js
+++ components/Menu.js
@@ -5,16 +5,16 @@ const Menu = (props) => {
   return (
     <ul className={ classes } role="navigation">
       <li className="nav__item">
-        <a href="mailto:colinxr@gmail.com">Email</a>
+        <a href="http://mailto:colinxr@gmail.com">Email</a>
       </li>
       <li className="nav__item">
-        <a href="github.com/colinxr" target="_blank">GitHub</a>
+        <a href="http://github.com/colinxr" target="_blank">GitHub</a>
       </li>
       {/* <li className="nav__item">
         <a href="medium.com/colinxr" target="_blank">Medium</a>
       </li> */}
       <li className="nav__item">
-        <a href="linkedIn.com/colinxr" target="_blank">LinkedIn</a>
+        <a href="http://linkedIn.com/colinxr" target="_blank">LinkedIn</a>
       </li>
       {
         !props.isSecondary && 
